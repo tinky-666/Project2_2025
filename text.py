6a9cafc47e665d51bf6c8d442bafdf326e00ee8c
tinky-666 <1837581774@qq.com>
2025-04-24 13:40:41 +0800

    Initial commit of plant humidity sensor project

---
+++ text.py
@@ -0,0 +1,18 @@
+import RP1.GPIO as GPIO
+import time
+#GPIO SETUP
+channel = 4
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.IN)
+
+def callback(channel):
+       if GPIO.input(channel):
+                   print ("Water Detected!")
+       else:
+                   print ("Water Detected!")
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncettime=300) # let us know when the pin goes HIGH orLOW
+GPIO.add_event_callback(channel, callback) # assign function to GPIO PIN, Run function on change
+# infinite loop
+while True:
+               time.sleep(0)
+
